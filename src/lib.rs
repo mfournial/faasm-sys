69e337f853e22b7d379006bd088396087e4a6dec
Mayeul Fournial <mfournial@outlook.com>
2020-03-07 14:02:16 +0000

    Add get_thread_num function for more testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 pub mod omp {
     extern "C" {
         fn omp_get_num_threads() -> i32;
+        fn omp_get_thread_num() -> i32;
+    }
+
+    pub fn get_thread_num() -> i32 {
+        unsafe { omp_get_thread_num() }
     }
 
     pub fn get_num_threads() -> i32 {
