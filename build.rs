fe2aae6220c53375f96c6c781e2ff998f65559f2
Mayeul Fournial <mfournial@outlook.com>
2020-03-08 00:44:38 +0000

    Improve build script
    * Tries to generate bindgens
    * Got tested with adding libc, libm, etc. too. libc name conflicts on
    `memcpy`, `memset`, `mem.*`

--- build.rs
+++ build.rs
@@ -1,7 +1,48 @@
-fn main() {
+use std::{env::var, fs::copy, path::{PathBuf}};
 
-    // Faasm default lib location
-    println!("cargo:rustc-link-search=/usr/local/faasm/llvm-sysroot/lib");
-    println!("cargo:rustc-link-lib=static=faasm");
-    println!("cargo:rustc-link-lib=static=faasmp");
+const FAASM_INCLUDE_WRAPPER: &str = "wrapper.h";
+const FAASM_VENDOR_FOLDER: &str = "vendor/faasm";
+
+fn main() {
+    let header = format!("{}/{}", FAASM_VENDOR_FOLDER, FAASM_INCLUDE_WRAPPER);
+
+    println!("cargo:rerun-if-changed={}", header);
+
+    let target = var("TARGET").unwrap();
+    if target == "wasm32-unknown-unknown" {
+        // Default Faasm libs location
+        println!("cargo:rustc-link-search=/usr/local/faasm/llvm-sysroot/lib");
+
+        // Add libraries
+        println!("cargo:rustc-link-lib=static=faasm");
+        println!("cargo:rustc-link-lib=static=faasmp");
+    }
+
+    // Included bindings were originally generated with the bindgen command line
+    // This would be a more streamlined process
+    let vendor_folder = PathBuf::from(FAASM_VENDOR_FOLDER);
+    let manual_gen_file = vendor_folder.join("bindings.rs");
+
+    // Location that can be included in the libray code
+    let binding_file = PathBuf::from(var("OUT_DIR").unwrap()).join("bindings.rs");
+
+    if manual_gen_file.exists() {
+        copy(manual_gen_file, binding_file).unwrap();
+    } else {
+        unimplemented!("FIXME: Implement dynamic bindings generation");
+        /*
+        std::env::set_var("LLVM_CONFIG_PATH", "/usr/lib/llvm-6.0/bin/llvm-config");
+        let bindings = bindgen::Builder::default()
+            .header(header)
+            // Only generate if wrapper was modified
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
+
+        bindings
+            .write_to_file(binding_file)
+            .expect("Couldn't write bindings!");
+        */
+    }
+
 }
