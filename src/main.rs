880c81a64df8296aac1e1b7f7ed9d9b1ef3e1808
Simon Shillaker <mail@simonshillaker.com>
2020-03-24 13:24:06 +0000

    Output msg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use faasm_sys::*;
 
+// ---------------------------
+// Simple test function
+// ---------------------------
+
 // Replace println macro in Wasm
 #[cfg(target_arch = "wasm32")]
 use faasm_sys::println;
 
 fn main() {
-    println!("Hello from Faasm");
+    println!("faasm-sys works");
 }
