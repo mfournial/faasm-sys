c80d02975086a08dbeace2fda9d9cde021604b73
Mayeul Fournial <mfournial@outlook.com>
2020-03-24 23:05:57 +0000

    Implement dev and install mode

--- README.md
+++ README.md
@@ -4,36 +4,25 @@ Rust bindings for [Faasm](https://github.com/lsds/Faasm).
 
 ## Build
 
-First we need the latest Faasm sysroot, this can be downloaded with:
+### Normal mode
 
-```bash
-./bin/download.sh
-```
-
-You can then run the build with:
+Running the following will:
+1. Download faasm libs and store them in `vendor`
+2. Include the `bindings.rs` file in `vendor` in the library
+3. Build the Wasm library
 
 ```bash
 cargo build --target wasm32-unknown-unknown
 ```
 
-## Running the test function
+### Local development mode
 
-To build and run the test function you need a Faasm 
-[local development](https://github.com/lsds/Faasm/blob/master/docs/local_dev.md) set up,
-and can use the script:
+If you set the `FAASM_SYS_DEV` environment variable before building, then the build script will fetch libraries according to the [Faasm local development](https://github.com/lsds/Faasm/blob/master/docs/local_dev.md) defaults. You can then run the test script as well:
 
 ```bash
+env FAASM_SYS_DEV=1 cargo build --target wasm32-unknown-unknown
 ./bin/test.sh
 ```
 
 Note that when used as a submodule of [rust-faasm](https://github.com/mfournial/rust-faasm), 
 the `target` directory is directed one level up.
-
-## `assert_eq` 
-
-Although panics do not look pretty when they happen in Wasm, `main` uses the `assert_eq` macro for
-example to check the value of an expression.
-
-```rust
-assert_eq!(omp::get_num_threads(), 1);
-```
--- build.rs
+++ build.rs
@@ -1,18 +1,139 @@
-use std::{env::var, fs::copy, path::PathBuf};
+use flate2::read::GzDecoder;
+use std::{env::var, fs, io, io::prelude::*, io::ErrorKind::AlreadyExists, path::PathBuf};
+use tar::Archive;
 
-const FAASM_INCLUDE_WRAPPER: &str = "wrapper.h";
 const FAASM_VENDOR_FOLDER: &str = "vendor/faasm";
-const FAASM_VENDOR_LIBS_FOLDER: &str = "vendor/faasm-libs";
+//const FAASM_RELEASE_BASE_URL: &str = "https://github.com/lsds/Faasm/releases/download/v";
+const FAASM_RELEASE_BASE_URL: &str = "https://www.doc.ic.ac.uk/~mmf115/v";
+const FAASM_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn main() {
-    let header = format!("{}/{}", FAASM_VENDOR_FOLDER, FAASM_INCLUDE_WRAPPER);
+enum FaasmRelease {
+    RuntimeRoot,
+    Sysroot,
+    Toolchain,
+}
 
-    println!("cargo:rerun-if-changed={}", header);
+impl FaasmRelease {
+    fn tar_filename(&self) -> String {
+        match self {
+            Self::RuntimeRoot => format!("faasm-runtime-root-{}.tar.gz", FAASM_VERSION),
+            Self::Sysroot => format!("faasm-sysroot-{}.tar.gz", FAASM_VERSION),
+            Self::Toolchain => format!("faasm-toolchain-{}.tar.gz", FAASM_VERSION),
+        }
+    }
+
+    fn filename(&self) -> String {
+        match self {
+            Self::RuntimeRoot => format!("runtime-root-{}", FAASM_VERSION),
+            Self::Sysroot => format!("sysroot-{}", FAASM_VERSION),
+            Self::Toolchain => format!("toolchain-{}", FAASM_VERSION),
+        }
+    }
+
+    fn file_path(&self) -> PathBuf {
+        PathBuf::from(FAASM_VENDOR_FOLDER).join(self.filename())
+    }
+
+    fn url(&self) -> String {
+        format!(
+            "{}{}/{}",
+            FAASM_RELEASE_BASE_URL,
+            FAASM_VERSION,
+            self.tar_filename()
+        )
+    }
+
+    fn download_tar(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let out_path = self.file_path();
+        if !out_path.exists() {
+            // Downloads file
+            let response = reqwest::blocking::get(&self.url())?;
+
+            // Extacts tar.gz
+            let tar = GzDecoder::new(response);
+            let mut archive = Archive::new(tar);
+            archive.unpack(out_path)?;
+        }
+
+        // We're done
+        Ok(())
+    }
+}
+
+// Generates a wrapper header pointing to the FaasmRelease sysroot
+fn generate_header(sysroot_path: &str) -> io::Result<String> {
+    let header_file = format!("{}/wrapper-{}.h", FAASM_VENDOR_FOLDER, FAASM_VERSION);
+    match fs::File::create(&header_file) {
+        Ok(mut header_handler) => {
+            let header_content = format!(
+                "\
+                #include \"{}/llvm-sysroot/include/faasm/host_interface.h\"\n\
+                #include \"{}/llvm-sysroot/include/faasm/rfaasm.h\"\
+            ",
+                sysroot_path, sysroot_path
+            );
+            header_handler.write_all(&header_content.into_bytes())?;
+            Ok(header_file)
+        }
+        Err(err) if err.kind() == AlreadyExists => Ok(header_file),
+        Err(err) => Err(err),
+    }
+}
+
+// TODO - implement bindings using bindgen
+fn generate_bindings(_wrapper: &str, output_file: &PathBuf) -> io::Result<()> {
+    // Warn that dynamic binding generation is not implemented and needs to be ran
+    // a-priori of build from the cmd line with bindgen.
+    println!("cargo:warning=Using manually generated bindings");
+
+    // Bindings file maintained manually in this repo
+    let vendor_folder = PathBuf::from(FAASM_VENDOR_FOLDER);
+    // Output file where we want to link things
+    let manual_gen_file = vendor_folder.join("bindings.rs");
+
+    // Location that can be included in the libray code
+    fs::copy(manual_gen_file, output_file).unwrap();
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Location of the binding file that will be included in the library
+    let binding_file = PathBuf::from(var("OUT_DIR").unwrap()).join("bindings.rs");
 
     let target = var("TARGET").unwrap();
     if target == "wasm32-unknown-unknown" {
-        // Link libs from Faasm sysroot
-        println!("cargo:rustc-link-search={}/llvm-sysroot/lib", FAASM_VENDOR_LIBS_FOLDER);
+        // Determine if we want the dev environment or a simple installation
+        let (library_path, header) = match var("FAASM_SYS_DEV") {
+            Ok(_) => (
+                "/usr/local/faasm/llvm-sysroot/lib".to_string(),
+                format!("{}/dev_wrapper.h", FAASM_VENDOR_FOLDER),
+            ),
+            Err(_) => {
+                // Download the Sysroot if it doesn't exists
+                FaasmRelease::Sysroot.download_tar()?;
+                let library_path = std::env::current_dir()?
+                    .join(FaasmRelease::Sysroot.file_path())
+                    .join("llvm-sysroot/lib");
+
+                // Paths can be invalid UTF-8 but we
+                let library_dir = library_path.into_os_string().into_string().unwrap();
+
+                // Generate a wrapper based on the library location
+                let header = generate_header(FaasmRelease::Sysroot.file_path().to_str().unwrap())?;
+
+                (library_dir, header)
+            }
+        };
+
+        // Rerun if the wrapper is changed (more relevant for dev mode)
+        println!("cargo:rerun-if-changed={}", header);
+
+        // TODO - this only copies the manually generated bindings
+        generate_bindings(&header, &binding_file)?;
+
+        // Link libs from Faasm Sysroot
+        println!("cargo:rustc-link-search={}", library_path);
 
         // Add libraries
         println!("cargo:rustc-link-lib=static=faasm");
@@ -20,30 +141,5 @@ fn main() {
         unimplemented!("Link native Faasm libraries");
     }
 
-    // Included bindings were originally generated with the bindgen command line
-    // This would be a more streamlined process
-    let vendor_folder = PathBuf::from(FAASM_VENDOR_FOLDER);
-    let manual_gen_file = vendor_folder.join("bindings.rs");
-
-    // Location that can be included in the libray code
-    let binding_file = PathBuf::from(var("OUT_DIR").unwrap()).join("bindings.rs");
-
-    if manual_gen_file.exists() {
-        copy(manual_gen_file, binding_file).unwrap();
-    } else {
-        unimplemented!("FIXME: Implement dynamic bindings generation");
-        /*
-        std::env::set_var("LLVM_CONFIG_PATH", "/usr/lib/llvm-6.0/bin/llvm-config");
-        let bindings = bindgen::Builder::default()
-            .header(header)
-            // Only generate if wrapper was modified
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-            .generate()
-            .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(binding_file)
-            .expect("Couldn't write bindings!");
-        */
-    }
+    Ok(())
 }
